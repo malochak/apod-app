c8d1106d6b4f2dbed8c79e0da2c8654b4b774dc1
pankeny <konrad_malocha@hotmail.com>
2019-05-06 20:41:59 +0200

    [apd008] add simple tab navigator

--- App.js
+++ App.js
@@ -1,31 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View  } from 'react-native';
-import { createMaterialBottomTabNavigator, createAppContainer } from 'react-navigation';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 import ApodScreen from './src/screens/ApodScreen';
 import LogonScreen from './src/screens/LogonScreen';
+import Icon from 'react-native-vector-icons/Ionicons'
 
-
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <HomeScreen />
-//     );
+// export default class App extends React.Component{
+//   render(){
+//     return(
+//         <TabNavigator />
+//     )
 //   }
 // }
 
 const TabNavigator = createMaterialBottomTabNavigator({
-  Apod: { screen: ApodScreen },
-  LogIn: { screen: LogonScreen}
-});
+  Apod: { screen: ApodScreen,
+          navigationOptions: {
+            tabBarLabel: 'Home',
+            tabBarIcon: ({tintColor}) => (
+              <Icon name='ios-home' color={tintColor} size={24} />
+            )
+          }},
+  Logon: { screen: LogonScreen,
+            navigationOptions: {
+              tabBarLabel: 'Sign In',
+              tabBarIcon: ({tintColor}) => (
+                <Icon name='ios-log-in' color={tintColor} size={24} />
+              )
+            }},
+}, {
+  initialRouteName: 'Apod',
+  activeTintColor: 'white',
+  activeColor: 'black',
+  inactiveColor: 'orange',
+  barStyle: { backgroundColor: '#FFF' }
+})
 
 export default createAppContainer(TabNavigator);
-
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
