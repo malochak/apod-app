ca5ea4aecdc6d00236f0b8a0ef6bb118eb96834b
KamilSzewczyk77 <k.szewczyk107@gmail.com>
2019-05-16 20:27:57 +0200

    apd034 logic to get favourites

--- src/screens/TopApodsScreen.js
+++ src/screens/TopApodsScreen.js
@@ -1,26 +1,65 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet,Image,ScrollView } from 'react-native';
+import { firebase } from '../components/logon/authentication_logic';
 
 
-export default class FavouriteScreen extends Component {
+export default class TopApodScreen extends Component {
+
+ 
+  readUserData() {
+    firebase.database().ref(`users/favourites/${userId}`).on('value', snapshot => {
+
+      this.setState({
+        favourites: snapshot.val()
+      })
+    });
+  }
+ 
 
   render() {
+   
+    /*var keyNames = null;
+    let favour = [];
+    let fullList;
+
+    keyNames = Object.keys(this.state.favourites);
+
+    if(keyNames !== null){
+          keyNames.forEach(e=>{
+            favour.push(
+              <div> <img src={this.state.favourites[e]}  alt=""
+              /> </div>
+            )
+          })
+
+          fullList = keyNames.map(e => (
+              <div className="col-md-3 my-3" key={e} style={{display:'inline-block'}}>
+                <img className='media-object' id="favourites"
+                 src={this.state.favourites[e]}
+                alt="" style={{width:'75%', border:'1px solid white'}} />
+              </div>
+            ))
+    }*/
     return (
-      <View style={styles.container}>
-       
-            <Text>Gallery</Text>
-          
-      </View>
+     
+      <ScrollView style={styles.container} >
+     
+        <View title = {this.state.favourites.apodData.title}>
+            
+            </View>
+      </ScrollView>
+        
+     
     );
   }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1 ,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
+      alignItems: 'center',
+      flex: 1,
+      justifyContent: 'center',
+      height: 250
     }
 
 })
