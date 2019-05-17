9dc5e4ad23f0d0ab080a5ee7372f04c661fbd8dd
KamilSzewczyk77 <k.szewczyk107@gmail.com>
2019-05-17 18:53:29 +0200

    apd013 Logic to TopApods

--- src/screens/TopApodsScreen.js
+++ src/screens/TopApodsScreen.js
@@ -1,24 +1,65 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
-import { auth } from '../components/logon/authentication_logic/';
+import { View, Text, StyleSheet,Image,ScrollView,Alert } from 'react-native';
+import { firebase } from '../components/logon/authentication_logic';
+import TopApods from '../components/favs/TopApods.js';
 
 export default class TopApodsScreen extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        apodData: ''
+      };
+   }
 
-
+  componentDidMount() {
+    var userId = firebase.auth.currentUser.uid;
+    firebase.app.database().ref(`apods/`).orderByChild('likes').limitToLast(10).on('value', snapshot => {
+      this.setState({
+        apodData: snapshot.val()
+      })
+    });
+  }
+  
+  
   render() {
-    return (
-      <View style={styles.container}>
-            <Text >TopApods</Text>
-      </View>
-    );
+    if (this.state.apodData !== '') {
+        if (this.state.apodData != null) {
+            var keyNames = Object.keys(this.state.apodData);
+            const items = [];
+            
+            keyNames.forEach(item => {
+                var favItem = this.state.apodData[item];
+                items.push(<TopApods
+                    key = {favItem.date}
+                    url = {favItem.url}
+                    title = {favItem.title}
+                    date = {favItem.date}
+                    likes = {favItem.likes}
+                             />)
+            });
+
+            return (
+              <ScrollView style={styles.container} >
+                {items}
+              </ScrollView>
+            );
+        }else {
+            return (
+                <Text> Like sth  </Text>
+            );
+        }
+    } else {
+         /* @TODO
+         */
+        return (
+            <Text> WAIT </Text>
+        )
+    }
   }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1 ,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
-    },
+      flex: 1,
+    }
 })
