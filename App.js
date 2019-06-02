3e260cf06fd56ec1b6534e86387448071aeef5b8
pankeny <konrad_malocha@hotmail.com>
2019-06-02 20:37:15 +0200

    change bg color

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#262e3d'
+    backgroundColor: '#2c3e50'
   },
 });
 
